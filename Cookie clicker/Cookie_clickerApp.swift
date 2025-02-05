a02af2ac603fe0c23a4414ccbd9ff12c887b1d74
Redmar Alkema <rag.alkema@student.avans.nl>
2025-02-05 12:08:54 +0100

    Initial Commit

---
+++ Cookie clicker/Cookie_clickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_clickerApp.swift
+//  Cookie clicker
+//
+//  Created by Redmar Alkema on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cookie_clickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cookie clickerTests/Cookie_clickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_clickerTests.swift
+//  Cookie clickerTests
+//
+//  Created by Redmar Alkema on 05/02/2025.
+//
+
+import Testing
+@testable import Cookie_clicker
+
+struct Cookie_clickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
