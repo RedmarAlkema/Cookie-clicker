40290b4bffa2c13be21ae36fcef25ee733c452db
Redmar Alkema <rag.alkema@student.avans.nl>
2025-02-07 09:54:19 +0100

    fix vorige gemiste commit

--- Cookie clicker/ContentView.swift
+++ Cookie clicker/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var timesClicked: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Cookie Clicker")
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: {
+                print("Click cookie")
+                timesClicked += 1
+            }) {
+                Image("cookie")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150) // Adjust size if needed
+            }
+            .padding()
+           
+            if timesClicked > 0 {
+                Text("Click the cookie! \(timesClicked)")
+                    .font(.title2)
+                    .padding()
+            }
+            else {
+                Text("Click the cookie!")
+            }
         }
-        .padding()
     }
 }
 
